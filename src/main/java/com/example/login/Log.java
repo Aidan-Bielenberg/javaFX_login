bfe3d2d699713613f5e441d6269eb9d172db4f14
aidan <aidanbielenberg@gmail.com>
2023-05-19 13:21:31 -0700

    :)
    Signed-off-by: aidan <aidanbielenberg@gmail.com>

--- src/main/java/com/example/login/Log.java
+++ src/main/java/com/example/login/Log.java
@@ -3,6 +3,7 @@ package com.example.login;
 import java.io.CharArrayWriter;
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.ArrayList;
 
 public class Log {
@@ -10,7 +11,14 @@ public class Log {
 
 
     public static void AppendToLog(String toLog) {
-        logFile.add(toLog);
+        try{
+            logFile.add(toLog);
+            PrintStream logOut = new PrintStream(new File("log.txt"));
+            logOut.println(logFile);
+        }
+        catch (IOException e) {
+            System.out.println(e);
+        }
     }
 
     public static void Dump() {
